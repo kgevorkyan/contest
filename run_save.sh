4e1fb5177e7ca14ce8d9d3f98bbff30113a1c587
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2023-09-22 17:13:12 +0300

    Polishing (#3)
    * polish

--- run_save.sh
+++ run_save.sh
@@ -9,16 +9,22 @@ BUILD_DIR="./example-analyzer/build"
 BITCODE_DIR="./bc_dir"
 REPORT_DIR="./reports"
 
+FILE_NAME_WITH_EXTENSION=${FILE_PATH##*/}
+FILE_NAME=${FILE_NAME_WITH_EXTENSION%.*}
+
+TEST_SUITE_PATH=$(dirname $(dirname "$FILE_PATH"))
+TEST_SUITE_NAME=${TEST_SUITE_PATH##*/}
+
+UNIQUE_NAME="${TEST_SUITE_NAME}_$FILE_NAME"
+
 cd $BITCODE_DIR
 
-clang -g -O0 -c -emit-llvm ${FILE_PATH}
+clang-12 -g -O0 -c -emit-llvm ${FILE_PATH} -o "$UNIQUE_NAME.bc"
 
 cd -
 
-FILE_NAME=${FILE_PATH##*/}
+opt-12 -load-pass-plugin ${BUILD_DIR}/src/libAnalyzer.so -passes=simple -disable-output "$BITCODE_DIR/${UNIQUE_NAME}.bc"
 
-opt-14 -load-pass-plugin ${BUILD_DIR}/src/libAnalyzer.so -passes=simple -disable-output "$BITCODE_DIR/${FILE_NAME%.*}.bc"
-
-REPORT="$FILE_NAME.sarif"
+REPORT="$UNIQUE_NAME.sarif"
 
 cp "report.sarif" "$REPORT_DIR/$REPORT" || echo "failed to run on $FILE_NAME"
